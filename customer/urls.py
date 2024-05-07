57fd564b5f9d0928d4f640c92c53c1731f4f2b95
Gleb <glebpand@gmail.com>
2024-05-07 23:41:17 +0300

    New back added ,new functions added,media added

--- customer/urls.py
+++ customer/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('',Index.as_view(),name='index'),
     path('about/', About.as_view(),name ='about'),
+    path('order/',Order.as_view(),name='order'),
 ]
